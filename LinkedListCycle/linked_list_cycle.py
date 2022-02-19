26e68449a8f800971c39e2df9fd39b6db592fc3c
Carol Chen <carolchen1120@yahoo.com>
2022-02-19 11:37:45 -0500

    fixed linked_list_cycle

--- LinkedListCycle/linked_list_cycle.py
+++ LinkedListCycle/linked_list_cycle.py
@@ -1,17 +1,13 @@
 # Definition for singly-linked list.
-from operator import truediv
-from pickle import TRUE
-
-
 class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
 
 class Solution:
-    def hasCycle(self) -> bool:
-        fast_curr = self.head
-        slow_curr = self.head
+    def hasCycle(self, head: ListNode) -> bool:
+        fast_curr = head
+        slow_curr = head
         while fast_curr and fast_curr.next and slow_curr:
             fast_curr = fast_curr.next.next
             slow_curr = slow_curr.next
