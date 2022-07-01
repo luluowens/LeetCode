86eb478a04f6a04c15d054d002cd2479d1521518
Carol Chen <carolchen1120@yahoo.com>
2022-07-01 18:22:12 -0400

    added docstring

--- Problems/str_str.py
+++ Problems/str_str.py
@@ -1,3 +1,10 @@
+'''Implement strStr().
+
+Given two strings needle and haystack,
+return the index of the first occurrence of needle in haystack,
+or -1 if needle is not part of haystack.
+'''
+
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         for i in range(len(haystack)-len(needle)+1) :
