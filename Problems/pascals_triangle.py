9e5d27cca58aa089579926dfcac7584fb218b068
Carol Chen <carolchen1120@yahoo.com>
2022-07-02 16:35:03 -0400

    wrote pascals triangle

---
+++ Problems/pascals_triangle.py
@@ -0,0 +1,35 @@
+'''Given an integer numRows, return the first numRows of Pascal's triangle.
+'''
+
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        cache = []
+
+        for num in range(numRows) :
+            if num == 0 :
+                cache.append([1])
+            else :
+                row = []
+                row.append(1)
+                previous_row = cache[num - 1]
+                for i in range(1, num) :
+                    row.append(previous_row[i-1] + previous_row[i])
+                row.append(1)
+                cache.append(row)
+        
+        return cache
+
+
+import unittest
+
+class TestLinkedList(unittest.TestCase) :
+
+    def test_solution(self) :
+        my_sol = Solution()
+        self.assertEqual(my_sol.generate(1), [[1]])
+        self.assertEqual(my_sol.generate(2), [[1], [1,1]])
+        self.assertEqual(my_sol.generate(5), [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]])
+        
+
+if __name__ == '__main__':
+    unittest.main()
